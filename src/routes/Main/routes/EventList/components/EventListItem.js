c1740b66299eaf012127c26b60943b20555a0228
Jonas Olander <jonas.olander91@gmail.com>
2017-02-27 09:17:42 +0100

    Updated database rules .

--- src/routes/Main/routes/EventList/components/EventListItem.js
+++ src/routes/Main/routes/EventList/components/EventListItem.js
@@ -6,14 +6,13 @@ import Pick from 'react-icons/lib/md/chevron-right';
 export default props => {
     const { name, active, eventId } = props;
     return (
-        <li className={styles.eventListItem}>
-            <div className={styles.itemContent}>
-                <h3>{name}</h3>
-                <span className={styles.divider} />
-                <Link to={`/main/event/${eventId}`}>
-                    <Pick color="#34495e" size="42" />
-                </Link>
-            </div>
-        </li>
+        <Link className={styles.eventLink} to={`/main/event/${eventId}`}>
+            <li className={styles.eventListItem}>
+                <div className={styles.itemContent}>
+                    <h3>{name}</h3>
+                    <span className={styles.divider} />
+                </div>
+            </li>
+        </Link>
     )
 }
